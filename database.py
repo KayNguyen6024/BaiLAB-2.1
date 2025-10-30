7845d81d70f1dbb728606dc03e720e104fadbe90
Nguyen Phuoc Thinh <kaynguyen2405@gmail.com>
2025-10-30 10:57:04 +0700

    Add files via upload

---
+++ database.py
@@ -0,0 +1,26 @@
+import mysql.connector
+from datetime import datetime
+
+class Database:
+    def __init__(self):
+        self.conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Kay240506",
+            database="course_registration"
+        )
+        self.cursor = self.conn.cursor()
+
+    def execute_query(self, query, params=None):
+        if params:
+            self.cursor.execute(query, params)
+        else:
+            self.cursor.execute(query)
+        self.conn.commit()
+
+    def fetch_all(self, query, params=None):
+        if params:
+            self.cursor.execute(query, params)
+        else:
+            self.cursor.execute(query)
+        return self.cursor.fetchall()
